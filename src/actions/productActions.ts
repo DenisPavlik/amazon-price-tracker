5ce0e906ff884a46befff91e062202449280ce42
Denys Pavlyk <denispavlik9@gmail.com>
2025-07-03 12:48:36 -0400

    Add puppeteer functionality

--- src/actions/productActions.ts
+++ src/actions/productActions.ts
@@ -1,5 +1,33 @@
 'use server'
+import puppeteer from "puppeteer";
 
 export async function addProduct(productId: string) {
-  
+  const url = `https://www.amazon.com/dp/${productId}`;
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  const data = await page.evaluate(() => {
+    const title = document.querySelector("#productTiltle")?.textContent?.trim()
+
+    const priceString = document.querySelector(".a-price .a-offscreen")?.textContent?.replace("$", "") || "0";
+    const price = parseFloat(priceString);
+
+    const img = document.querySelector("#landingImage")?.getAttribute("src");
+
+    const reviewsCountString = document.querySelector("#acrCustomerReviewText")?.textContent?.split(" ")[0].replace(",", "") || "0";
+    const reviewsCount = parseInt(reviewsCountString);
+
+    const ratingString = document.querySelector(".a-icon-alt")?.textContent?.split(" ")[0] || "0";
+    const reviewsAverageRating = parseFloat(ratingString);
+
+    return { title, price, img, reviewsCount, reviewsAverageRating };
+  })
+
+  await browser.close();
+
+  return data;
 }
--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,13 +1,23 @@
+import { addProduct } from "@/actions/productActions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export default function AddProduct() {
+  async function handleSubmit (e:React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const productId = formData.get("productId") as string;
+
+    const response = await addProduct(productId);
+    console.log(response);
+    
+  }
   return (
     <div className="col-span-9">
       <div className="max-w-md mx-auto bg-white/50 rounded-xl p-6">
         <h1 className="font-bold mb-2 text-center">Add product</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Label
             htmlFor="productId"
             className="mb-1 uppercase font-extrabold text-gray-600"
@@ -16,6 +26,7 @@ export default function AddProduct() {
           </Label>
           <Input
             id="productId"
+            name="productId"
             placeholder="Enter productID, example: L02IF44P817C"
           />
           <div className="flex justify-center mt-6">
